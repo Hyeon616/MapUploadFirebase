6bde71e718534a2d2872ae5f2d41572caf418610
우종현 <whdgus616@gmail.com>
2024-09-10 07:53:38 +0900

    [feat] 경로문제 수정

--- MapUploadFirebase/Program.cs
+++ MapUploadFirebase/Program.cs
@@ -3,28 +3,41 @@
     public static async Task Main(string[] args)
     {
         var logger = new ConsoleLogger();
-        var httpClient = new HttpClient();
-        var firebaseUrl = "https://enpconventionproject-5ff8f-default-rtdb.firebaseio.com/";
-        var firebaseUploader = new FirebaseUploader(httpClient, firebaseUrl, logger);
-        var excelProcessor = new ExcuteExcel(firebaseUploader, logger);
+        logger.Log("Program started.");
+        logger.Log($"Current Working Directory: {Directory.GetCurrentDirectory()}");
 
         try
         {
-            if (args.Length < 1)
+            string directoryPath = Directory.GetCurrentDirectory();
+            logger.Log($"Using Directory: {directoryPath}");
+
+            string uploadType;
+            do
             {
-                logger.Log("Please provide the upload type (chapter or answer) as an argument.");
-                return;
+                Console.Write("Enter upload type (chapter or answer): ");
+                uploadType = Console.ReadLine().ToLower();
+            } while (uploadType != "chapter" && uploadType != "answer");
+
+            logger.Log($"Upload Type: {uploadType}");
+
+            // 디렉토리 내용 출력
+            logger.Log("Directory contents:");
+            foreach (string file in Directory.GetFiles(directoryPath, "*.xlsm"))
+            {
+                logger.Log($"  {Path.GetFileName(file)}");
             }
 
-            string uploadType = args[0].ToLower();
-            if (uploadType != "chapter" && uploadType != "answer")
-            {
-                logger.Log("Invalid upload type. Please use either 'chapter' or 'answer'.");
-                return;
-            }
+            var httpClient = new HttpClient();
+            var firebaseUrl = "https://enpconventionproject-5ff8f-default-rtdb.firebaseio.com/";
+            var firebaseUploader = new FirebaseUploader(httpClient, firebaseUrl, logger);
+            var excelProcessor = new ExcuteExcel(firebaseUploader, logger);
 
-            string currentDirectory = Directory.GetCurrentDirectory();
-            await excelProcessor.ProcessExcelFiles(currentDirectory, uploadType);
+            logger.Log($"Processing Excel files in directory: {directoryPath}");
+            logger.Log($"Upload type: {uploadType}");
+
+            await excelProcessor.ProcessExcelFiles(directoryPath, uploadType);
+
+            logger.Log("Processing complete.");
         }
         catch (Exception ex)
         {
@@ -32,7 +45,7 @@
             logger.Log($"Stack Trace: {ex.StackTrace}");
         }
 
-        logger.Log("Processing complete. Press any key to exit...");
+        logger.Log("Press any key to exit...");
         Console.ReadKey();
     }
 }
